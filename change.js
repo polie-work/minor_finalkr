01d530d06567c78d980934cc1dad89058efba9e1
lin <polie.epn@gmail.com>
2024-11-06 09:30:33 +0300

    Add files via upload

---
+++ change.js
@@ -0,0 +1,8 @@
+const changeText = () => 
+    {document.getElementsByClassName('vidanstr1')[0].textContent = "DEPARTMENT OF INTERNAL";
+        document.getElementsByClassName('vidanstr2')[0].textContent = "SAINT PETERSBURG";
+        document.getElementsByClassName('hfamilia')[0].textContent = "EVGRAFOVA";
+        document.getElementsByClassName('hname')[0].textContent = "POLINA";
+        document.getElementsByClassName('hfname')[0].textContent = "NIKOLAEVNA";
+        document.getElementsByClassName('hpo1')[0].textContent = "FEMALE";
+        document.getElementsByClassName('hpborn1')[0].textContent = "SAINT PETERSBURG";}
